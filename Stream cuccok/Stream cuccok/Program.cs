5bc8ce91372650602b6e0984c0548a51839ae478
CourtesyHUN <52625766+CourtesyHUN@users.noreply.github.com>
2023-01-17 13:57:50 +0100

    Add files via upload

---
+++ Stream cuccok/Stream cuccok/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stream_cuccok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Kell a using System.IO;*/
+            StreamReader sr = new StreamReader("eloadas.txt");
+            string sor = sr.ReadLine();
+            int sordb = 0;
+            while (sor != null)
+            {
+                Console.WriteLine(sor);
+                sor = sr.ReadLine();
+                sordb++;
+            }
+            Console.WriteLine(sordb);
+            Console.ReadKey();
+        }
+    }
+}
